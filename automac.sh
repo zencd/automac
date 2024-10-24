94c332aa757086c8796ed953c683d5d9d8c1888b
pasza <z@z.com>
2024-10-24 16:52:14 +0300

    multi-file

--- automac.sh
+++ automac.sh
@@ -33,12 +33,13 @@ if [ $# = 0 ]; then
   exit 1
 fi
 
-distr_url=https://github.com/zencd/automac/archive/v1.zip
+BRANCH=${BRANCH:-v1}
+DISTR_URL=https://github.com/zencd/automac/archive/${BRANCH}.zip
 venv=automac-venv
 conf_py=$1
 ensure_cli_tools_installed
 [ -d automac-venv ] || (set -x; python3 -m venv $venv)
 if ! $venv/bin/python3 -c 'import automac' 2>/dev/null; then
-  (set -x; $venv/bin/python3 -m pip install --no-cache-dir "$distr_url")
+  (set -x; $venv/bin/python3 -m pip install --no-cache-dir "DISTR_URL")
 fi
 (set -x; $venv/bin/python3 "$conf_py")
