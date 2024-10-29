05f877478c6391722a8d5647a37f862f03689e2d
pasza <z@z.com>
2024-10-29 16:45:56 +0300

    good package struct

--- automac.sh
+++ automac.sh
@@ -38,9 +38,13 @@ DISTR_URL=https://github.com/zencd/automac/archive/${BRANCH}.zip
 venv=venv-client
 conf_py=$1
 ensure_cli_tools_installed
-[ -d "$venv" ] || (set -x; python3 -m venv "$venv")
-$venv/bin/python3 -m pip install --upgrade pip
-if ! $venv/bin/python3 -c 'import automac' 2>/dev/null; then
-  (set -x; $venv/bin/python3 -m pip install --no-cache-dir --force-reinstall "$DISTR_URL")
-fi
+[ -d "$venv" ] || (
+  set -x
+  python3 -m venv "$venv"
+  $venv/bin/python3 -m pip install --upgrade pip
+  $venv/bin/python3 -m pip install --no-cache-dir --force-reinstall "$DISTR_URL"
+)
+#if ! $venv/bin/python3 -c 'import automac' 2>/dev/null; then
+#  (set -x; $venv/bin/python3 -m pip install --no-cache-dir --force-reinstall "$DISTR_URL")
+#fi
 (set -x; $venv/bin/python3 "$conf_py")
